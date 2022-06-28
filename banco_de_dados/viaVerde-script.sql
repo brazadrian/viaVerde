fcc8d7d9c339bbde832152ad7c9804dc1a2e999d
Adrian de Souza <gilmaradrian127@gmail.com>
2022-06-27 23:12:08 -0300

    Ajustes para apresentacao

--- banco_de_dados/viaVerde-script.sql
+++ banco_de_dados/viaVerde-script.sql
@@ -84,13 +84,13 @@ CREATE TABLE IF NOT EXISTS `viaVerde`.`markets_have_farmers` (
 
 USE `viaVerde`;
 
-INSERT INTO `viaVerde`.`clients` (`id_cpf`, `name`, `email`) VALUES ('69013032893', 'Rebecca Maria Marlene', 'becca@gmail.com');
-INSERT INTO `viaVerde`.`clients` (`id_cpf`, `name`, `email`) VALUES ('14268305300', 'Renan Lorenzo Sérgio Costa', 'renancosta@gmail.com');
-INSERT INTO `viaVerde`.`clients` (`id_cpf`, `name`, `email`) VALUES ('20341345407', 'Martin Pietro Diego da Mata', 'martinpdmata@gmail.com');
-INSERT INTO `viaVerde`.`clients` (`id_cpf`, `name`, `email`) VALUES ('44434759078', 'Maya Juliana da Paz', 'mayadapaz@hotmail.com');
-INSERT INTO `viaVerde`.`clients` (`id_cpf`, `name`, `email`) VALUES ('03289369013', 'Vanessa Alessandra da Luz', 'nessaluz@hotmail.com');
+INSERT INTO `viaVerde`.`clients` VALUES ('69013032893', 'Rebecca Maria Marlene', 'becca@gmail.com');
+INSERT INTO `viaVerde`.`clients` VALUES ('14268305300', 'Renan Lorenzo Sérgio Costa', 'renancosta@gmail.com');
+INSERT INTO `viaVerde`.`clients` VALUES ('20341345407', 'Martin Pietro Diego da Mata', 'martinpdmata@gmail.com');
+INSERT INTO `viaVerde`.`clients` VALUES ('44434759078', 'Maya Juliana da Paz', 'mayadapaz@hotmail.com');
+INSERT INTO `viaVerde`.`clients` VALUES ('03289369013', 'Vanessa Alessandra da Luz', 'nessaluz@hotmail.com');
 -- pra dar erro e testar tua fé:
-INSERT INTO `viaVerde`.`clients` (`id_cpf`, `name`, `email`) VALUES ('03289369013', 'Teste com mesmo CPF', 'testecpf@gmail.com'); -- mesmo cpf da anterior
+INSERT INTO `viaVerde`.`clients` VALUES ('03289369013', 'Teste com mesmo CPF', 'testecpf@gmail.com'); -- mesmo cpf da anterior
 
 -- -----------------------------------------------------
 -- consultar todos os clientes
@@ -98,21 +98,24 @@ INSERT INTO `viaVerde`.`clients` (`id_cpf`, `name`, `email`) VALUES ('0328936901
 SELECT * FROM `viaVerde`.`clients`;
 
 -- ALTERANDO clients
-
 UPDATE `clients`
 SET `email` = 'beccamm@gmail.com'
 WHERE `id_cpf` = '69013032893';
 
--- CONSULTANDO TODOS OS CLIENTES DE NOVO
-SELECT * FROM `viaVerde`.`clients`;
+-- -----------------------------------------------------
+-- consultar todos os clientes
+-- -----------------------------------------------------
+SELECT * FROM `viaVerde`.`clients`; -- observar que houve alteração em becca
 
--- POPULANDO farmers
-
-INSERT INTO `viaVerde`.`farmers` (`id_cpf`, `name`, `organic_certificate`, `email`) VALUES ('15334358944', 'Josiane Maria de Souza', True, 'josiane@fetape.com');
-INSERT INTO `viaVerde`.`farmers` (`id_cpf`, `name`, `organic_certificate`, `email`) VALUES ('13538944345', 'Jose', False, 'jose@fetape.com');
-INSERT INTO `viaVerde`.`farmers` (`id_cpf`, `name`, `organic_certificate`, `email`) VALUES ('03289369013', 'Vanessa Alessandra da Luz', True, 'nessaluz2@hotmail.com');
-INSERT INTO `viaVerde`.`farmers` (`id_cpf`, `name`, `organic_certificate`, `email`) VALUES ('44955616429', 'Katyene Cavalcanti Reis', True, 'katyene.reis@geradornv.com.br');
-INSERT INTO `viaVerde`.`farmers` (`id_cpf`, `name`, `organic_certificate`, `email`) VALUES ('86287626046', 'Benedito Livramento Cruz', True, 'benedito.cruz@geradornv.com.br');
+-- -----------------------------------------------------
+-- populando FARMERS
+-- -----------------------------------------------------
+INSERT INTO `viaVerde`.`farmers` VALUES ('15334358944', 'Josiane Maria de Souza', True, 'josiane@fetape.com');
+INSERT INTO `viaVerde`.`farmers` VALUES ('13538944345', 'José Ferreira da Costa', False, 'costaze@fetape.com');
+INSERT INTO `viaVerde`.`farmers` VALUES ('03289369013', 'Vanessa Alessandra da Luz', True, 'nessaluz2@hotmail.com');
+INSERT INTO `viaVerde`.`farmers` VALUES ('44955616429', 'Katyene Cavalcanti Reis', True, 'katyene.reis@geradornv.com.br');
+INSERT INTO `viaVerde`.`farmers` VALUES ('86287626046', 'Benedito Livramento Cruz', True, 'benedito.cruz@geradornv.com.br');
+INSERT INTO `viaVerde`.`farmers` VALUES ('12477634522', 'Marcos Antonio Silva', False, 'marcantonio@gmail.com');
 
 -- -----------------------------------------------------
 -- consultar quem tem certificado orgânico
@@ -128,10 +131,10 @@ SELECT `clients`.`id_cpf`, `farmers`.`id_cpf`
 		 on `clients`.`id_cpf` = `farmers`.`id_cpf`;
 
 -- populando MARKETS
-INSERT INTO `viaVerde`.`markets` (`name`, `adress`) VALUES ('FETAPE', 'R. Gervásio Pires, 876 - Santo Amaro, Recife - PE, 50050-070');
-INSERT INTO `viaVerde`.`markets` (`name`, `adress`) VALUES ('Santo Amaro', 'Rua Frei Cassimiro, SN - Santo Amaro, Recife - PE');
-INSERT INTO `viaVerde`.`markets` (`name`, `adress`) VALUES ('Casa Amarela', 'Rua Padre Lemos, 94 - Casa Amarela, Recife - PE');
-INSERT INTO `viaVerde`.`markets` (`name`, `adress`) VALUES ('Parnamirim', 'Rua do Parnamirim, SN - Parnamirim, Recife - PE');
+INSERT INTO `viaVerde`.`markets` VALUES (DEFAULT, 'FETAPE', 'R. Gervásio Pires, 876 - Santo Amaro, Recife - PE, 50050-070');
+INSERT INTO `viaVerde`.`markets` VALUES (DEFAULT, 'Santo Amaro', 'Rua Frei Cassimiro, SN - Santo Amaro, Recife - PE');
+INSERT INTO `viaVerde`.`markets` VALUES (DEFAULT, 'Casa Amarela', 'Rua Padre Lemos, 94 - Casa Amarela, Recife - PE');
+INSERT INTO `viaVerde`.`markets` VALUES (DEFAULT, 'Parnamirim', 'Rua do Parnamirim, SN - Parnamirim, Recife - PE');
 
 -- -----------------------------------------------------
 -- consultando a feira de santo amaro
@@ -148,7 +151,9 @@ INSERT INTO `viaVerde`.`operation_days` VALUES(4, '2022-06-27', '06:00:00', '11:
 -- -----------------------------------------------------
 -- consultar as feiras da data de hoje
 -- -----------------------------------------------------
-SELECT * FROM `viaVerde`.`operation_days` WHERE operation_day = '2022-06-27';
+SELECT * FROM `viaVerde`.`operation_days` WHERE operation_day = '2022-06-27'; -- data do dia da apresentação
+
+SELECT *, `name` FROM `viaVerde`.`operation_days`, `viaVerde`.`markets` WHERE operation_day = '2022-06-27'; -- seleciona, também, o nome das feiras de hoje
 
 -- POPULANDO markets_have_farmers
 INSERT INTO `viaVerde`.`markets_have_farmers` VALUES(1, '15334358944');
@@ -160,9 +165,9 @@ INSERT INTO `viaVerde`.`markets_have_farmers` VALUES(1, '86287626046');
 -- -----------------------------------------------------
 -- consultar quem é da feira da FETAPE (feira 1)
 -- -----------------------------------------------------
-SELECT * FROM `viaVerde`.`markets_have_farmers` WHERE `markets_id_market` = 1;
+SELECT *, `name` FROM `viaVerde`.`markets_have_farmers`, `viaVerde`.`farmers` WHERE `markets_id_market` = 1;
 
--- POPULANDO buy-VENDA
+-- POPULANDO BUY
 INSERT INTO `viaVerde`.`buys` (`clients_id_cpf`, `farmers_id_cpf`, `recue_data`, `markets_id_market`) VALUES('69013032893', '15334358944', '2022-06-27 15:35:00', 1);
 INSERT INTO `viaVerde`.`buys` (`clients_id_cpf`, `farmers_id_cpf`, `recue_data`, `markets_id_market`) VALUES('14268305300', '13538944345', '2022-06-27 10:48:00', 2);
 INSERT INTO `viaVerde`.`buys` (`clients_id_cpf`, `farmers_id_cpf`, `recue_data`, `markets_id_market`) VALUES('20341345407', '03289369013', '2022-06-27 17:40:00', 3);
